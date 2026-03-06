9c6802f8f9270182cacba6d12fcb64b114714d85
Yuwei Duan <dyf102@gmail.com>
2026-03-06 20:43:31 +0000

    fix: comment out failing Amplify and Cloudflare resources to unblock core deploy [apply]

--- terraform/environments/prod/dns.tf
+++ terraform/environments/prod/dns.tf
@@ -1,25 +1,25 @@
 # 1. Automate SSL Validation for CloudFront
-resource "cloudflare_record" "acm_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.toronto.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+# resource "cloudflare_record" "acm_validation" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.toronto.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
 
-  zone_id = var.cloudflare_zone_id
-  name    = each.value.name
-  value   = each.value.record
-  type    = each.value.type
-  proxied = false
-}
+#   zone_id = var.cloudflare_zone_id
+#   name    = each.value.name
+#   value   = each.value.record
+#   type    = each.value.type
+#   proxied = false
+# }
 
 # 2. Final Routing to CloudFront (Subpath router)
-resource "cloudflare_record" "toronto_app" {
-  zone_id = var.cloudflare_zone_id
-  name    = "toronto"
-  value   = aws_cloudfront_distribution.toronto.domain_name
-  type    = "CNAME"
-  proxied = true
-}
+# resource "cloudflare_record" "toronto_app" {
+#   zone_id = var.cloudflare_zone_id
+#   name    = "toronto"
+#   value   = aws_cloudfront_distribution.toronto.domain_name
+#   type    = "CNAME"
+#   proxied = true
+# }
